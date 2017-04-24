64d4766eb369062443e92779c820cfafd4db48e4
tjtjtjtj <nemunemutjtjtjtj@gmail.com>
2017-04-24 18:47:09 +0100

    update

--- mackerel-agent-control.sh
+++ mackerel-agent-control.sh
@@ -5,8 +5,8 @@ set -o pipefail
 IMAGE_NAME="mackerel/mackerel-agent"
 CONTAINER_NAME="mackerel-agent"
 
-func agent_container_run(){
-docker run -h `hostname` \
+agent_container_run() {
+  docker run -h `hostname` \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v /var/lib/mackerel-agent/:/var/lib/mackerel-agent/ \
     -e "apikey=${apikey}" \
@@ -18,71 +18,71 @@ docker run -h `hostname` \
     -d \
     -rm \
     ${IMAGE_NAME}
-RETVAL=$?
-[ ${RETVAL} -eq 0 ] || echo "docker run ${CONTAINER_NAME} failed."
-return ${RETVAL}
+  RETVAL=$?
+  [ ${RETVAL} -eq 0 ] || echo "docker run ${CONTAINER_NAME} failed."
+  return ${RETVAL}
 }
 
-func agent_container_stop(){
-docker stop ${CONTAINER_NAME}
-RETVAL=$?
-[ ${RETVAL} -eq 0 ] || echo "docker stop ${CONTAINER_NAME} failed."
-return ${RETVAL}
+agent_container_stop() {
+  docker stop ${CONTAINER_NAME}
+  RETVAL=$?
+  [ ${RETVAL} -eq 0 ] || echo "docker stop ${CONTAINER_NAME} failed."
+  return ${RETVAL}
 }
 
-func agent_container_status(){
-docker inspect \
-  --format="Status:{{.State.Status}} StartedAt:{{.State.StartedAt}} Image:{{.Image}}" \
-  ${CONTAINER_NAME} | tr " " "\n"
-RETVAL=$?
-[ ${RETVAL} -eq 0 ] || echo "docker inspect ${CONTAINER_NAME} failed."
-return ${RETVAL}
+agent_container_status() {
+  docker inspect \
+    --format="Status:{{.State.Status}} StartedAt:{{.State.StartedAt}} Image:{{.Image}}" \
+    ${CONTAINER_NAME} | tr " " "\n"
+  RETVAL=$?
+  [ ${RETVAL} -eq 0 ] || echo "docker inspect ${CONTAINER_NAME} failed."
+  return ${RETVAL}
 }
 
-func agent_image_delete(){
-docker images -f dangling=true  -q ${IMAGE_NAME} | docker rmi
-RETVAL=$?
-[ ${RETVAL} -eq 0 ] || echo "${CONTAINER_NAME} image delete failed."
-return ${RETVAL}
+agent_image_delete() {
+  docker images -f dangling=true  -q ${IMAGE_NAME} | docker rmi
+  RETVAL=$?
+  [ ${RETVAL} -eq 0 ] || echo "${CONTAINER_NAME} image delete failed."
+  return ${RETVAL}
 }
 
-func agent_image_pull(){
-IMAGES_LIST=`docker pull ${IMAGE_NAME}`
-RETVAL=$?
-[ ${retval} -eq 0 ] || echo "docker pull ${CONTAINER_NAME} image failed." ; exit ${retval}
+agent_image_pull() {
+  IMAGES_LIST=`docker pull ${IMAGE_NAME}`
+  RETVAL=$?
+  [ ${retval} -eq 0 ] || echo "docker pull ${CONTAINER_NAME} image failed." ; exit ${retval}
 
-if echo ${IMAGES_LIST} | grep -q "Image is up to date"; then
-  echo "${CONTAINER_NAME} image is up to date."
-  exit 0
-fi
-return 0
+  if echo ${IMAGES_LIST} | grep -q "Image is up to date"; then
+    echo "${CONTAINER_NAME} image is up to date."
+    exit 0
+  fi
+  return 0
 }
 
-func agent_container_update(){
-agent_image_pull
-agent_container_stop
-agent_container_run 
-agent_image_delete 
+agent_container_update() {
+  agent_image_pull
+  agent_container_stop
+  agent_container_run 
+  agent_image_delete 
 }
 
 argument check
 #ここで引数１個のかくにん
 
 case "$1" in
-      start)
-        agent_container_run
-        ;;
-      stop)
-        agent_container_stop
-        ;;
-      status)
-        agent_container_status
-        ;;
-      update)
-        agent_container_update
-        ;;
-      *)
-        echo $"Usage: $0 {start|stop|status|update}"
-        exit 2
-    esac
+  start)
+    agent_container_run
+    ;;
+  stop)
+    agent_container_stop
+    ;;
+  status)
+    agent_container_status
+    ;;
+  update)
+    agent_container_update
+    ;;
+  *)
+    echo $"Usage: $0 {start|stop|status|update}"
+    exit 2
+esac
 exit $?
